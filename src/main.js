e947f16782bd13130a926ccf7db13aff64465f04
Andriy Kovalenko <andriyworkkovalenko@gmail.com>
2024-08-24 09:33:29 +0300

    uhaaaa random images work

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { DATA } from "./data/data.js";
+  function getRandomPhotoURL(data) {
+    const photos = data;
+    const photoKeys = Object.keys(photos);
+  
+    const randomIndex = Math.floor(Math.random() * photoKeys.length);
+  
+    const randomPhotoKey = photoKeys[randomIndex];
+    const randomPhoto = photos[randomPhotoKey];
+    
 
-const spData = DATA.photos.photo1.URL;
-
+    return randomPhoto;
+}
+const spData = DATA.photos;
 export { spData };
+export {getRandomPhotoURL};
--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
-import express from 'express';
+import express, { Router } from 'express';
 import { spData } from '/home/kovalenko/my-app/my-backend/src/main.js';
+import { getRandomPhotoURL } from '/home/kovalenko/my-app/my-backend/src/main.js';
 const app = express();
 const port = 1234;
-let inputData = spData;
 
-// app.get('/api/photo')
 
 app.get('/', (req, res) => {
-  res.send(`дата після логіки: ${inputData}`);
+  res.send("Hello here server aanddark-website.vercel.app");
 });
-
-// Запускаємо сервер
+app.get('/api/photos' ,(req, res) => {
+  res.json(spData)
+})
+app.get('/api/random', (req, res) => {
+    const photos = getRandomPhotoURL(spData);
+    if(photos){
+      res.json(photos)
+    } else{
+      res.status(404).send("WTF image not found ERROR 404")
+    }
+});
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
